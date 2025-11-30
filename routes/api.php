d9c513ab9d637100268e93c331b45b5161b52379
Saga Mahyana <sagamahyana2@gmail.com>
2025-11-30 20:24:57 +0700

    Adding APIController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\TenantController;
@@ -21,7 +22,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
-
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
@@ -32,6 +32,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/items', [ItemController::class, 'store']);
     Route::put('/items/{id}', [ItemController::class, 'update']);
     Route::delete('/items/{id}', [ItemController::class, 'destroy']);
+
+    ////////////////////
+    /// ORDER ROUTES //
+    ////////////////////
+    
+    Route::post('/orders', [OrderController::class, 'store']);
 });
 
 ////////////////////
@@ -39,23 +45,20 @@ Route::middleware('auth:sanctum')->group(function () {
 ////////////////////
 
 // Tenant (public)
-Route::get('/tenants', [TenantController::class, 'index']);
+Route::get('/tenants', [ApiController::class, 'getTenants']);
+Route::get('/tenants/{id}', [ApiController::class, 'getTenantById']);
+Route::get('/tenants/{tenantId}/items', [ApiController::class, 'getItemsByTenant']);
 
 // Item (public)
 Route::get('/items', [ItemController::class, 'index']);
 Route::get('/items/{id}', [ItemController::class, 'show']);
-
 
 /////////////////////
 /// ADMIN ROUTES ///
 /////////////////////
 
 Route::middleware(['auth:sanctum', 'role:Admin'])->group(function () {
-
     Route::post('/tenants', [TenantController::class, 'storeTenants']);
     Route::put('/tenants/{id}', [TenantController::class, 'update']);
     Route::delete('/tenants/{id}', [TenantController::class, 'destroy']);
-
 });
-
- Route::post('/orders', [OrderController::class, 'store']);
